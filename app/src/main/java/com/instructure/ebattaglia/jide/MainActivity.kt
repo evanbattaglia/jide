64769ab25e0e08dccde188dc5ca1651916fb25d9
Evan Battaglia <ebattaglia@instructure.com>
2020-06-24 11:04:05 -0600

    playing around with rendering HTML
    but I think I will abandon this and go for just rendering individual note fields.
    can't really get anything good from rendering html

--- app/src/main/java/com/instructure/ebattaglia/jide/MainActivity.kt
+++ app/src/main/java/com/instructure/ebattaglia/jide/MainActivity.kt
@@ -14,6 +14,19 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.widget.Toast
 import com.ichi2.anki.FlashCardsContract
+import android.app.WallpaperManager
+import android.graphics.*
+import android.webkit.WebView
+import android.view.View
+import android.graphics.Bitmap
+import android.text.Html
+import android.text.StaticLayout
+import android.text.TextPaint
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.text.Layout
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -113,7 +126,7 @@ class MainActivity : AppCompatActivity() {
         cursor2.close()
 
         val cursor3 = contentResolver.query(Uri.parse("${FlashCardsContract.Note.CONTENT_URI}/${noteId}/cards/${ordId}"),
-            arrayOf(FlashCardsContract.Card.ANSWER_PURE, FlashCardsContract.Card.QUESTION_SIMPLE), null, null, null)
+            arrayOf(FlashCardsContract.Card.ANSWER, FlashCardsContract.Card.QUESTION, FlashCardsContract.Card.ANSWER_PURE, FlashCardsContract.Card.QUESTION_SIMPLE), null, null, null)
         cursor3.moveToFirst()
         for (i in 0 until cursor3.columnCount) {
             Log.e(
@@ -124,12 +137,98 @@ class MainActivity : AppCompatActivity() {
         }
         Log.e("question!!!", cursor3.getString(cursor3.getColumnIndex(FlashCardsContract.Card.QUESTION_SIMPLE)))
         Log.e("answer pure!!!", cursor3.getString(cursor3.getColumnIndex(FlashCardsContract.Card.ANSWER_PURE)))
-        cursor3.close()
 
         // TODO In Anki can we have different templates for different mediums that are still tracked the same for learning purposes as the same cards in the same deck?
         // Or, maybe we just have an "ord override" in our app (that gets weird with multiple decks though)
 
+        screenshot3(cursor3.getString(cursor3.getColumnIndex(FlashCardsContract.Card.ANSWER_PURE)))
+        cursor3.close()
 
+    }
+
+    private fun screenshot3(html: String) {
+        val spanned = Html.fromHtml(html, null, null)
+        val paint = TextPaint()
+        paint.setTextSize(20f);
+        paint.setColor(Color.RED);
+        val sl = StaticLayout(spanned, paint, 1080, Layout.Alignment.ALIGN_NORMAL, 1f, 0f, false)
+        val bitmap = Bitmap.createBitmap(
+            1080, 2220, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        sl.draw(canvas)
+        val wm = WallpaperManager.getInstance(applicationContext)
+        wm.setBitmap(bitmap, Rect(0, 0, bitmap.width, bitmap.height), true, WallpaperManager.FLAG_LOCK)
+    }
+
+    private fun screenshot2(html: String) {
+        val view = WebView(this)
+        val scale = 2.0f
+        val contentWidth = 240
+
+
+        view.setPictureListener(object: WebView.PictureListener {
+            override fun onNewPicture(view: WebView, picture: Picture?) {
+                if (view.getProgress() == 100 && view.contentHeight > 0) {
+                    view.setPictureListener(null)
+                    // Content is now fully rendered
+                    val width = Math.round(contentWidth * scale)
+                    val height = Math.round(view.getContentHeight() * scale)
+                    val bitmap = getBitmap(view, width, height)
+                    // Display or print bitmap...
+                    val wm = WallpaperManager.getInstance(applicationContext)
+                    wm.setBitmap(bitmap, Rect(0, 0, bitmap.width, bitmap.height), true, WallpaperManager.FLAG_LOCK)
+                }
+            }
+
+            private fun getBitmap(
+                view: WebView, width: Int, height: Int
+            ): Bitmap {
+                val bitmap = Bitmap.createBitmap(
+                    width, height, Bitmap.Config.ARGB_8888
+                )
+                val canvas = Canvas(bitmap)
+                view.draw(canvas)
+
+                return bitmap
+            }
+        });
+
+        view.loadData(html, "text/html", "UTF8")
+        //view.capturePicture().draw()
+
+        //view.setInitialScale(Math.round(scale * 100));
+        // Width and height must be at least 1
+        view.layout(0, 0, 1080, 2220);
+    }
+
+    private fun screenshot() {
+        val webView = WebView(this)
+        webView.loadData("<html><body><h1>Hello world</h1></body></html>", "text/html", "UTF8")
+        webView.measure(
+            View.MeasureSpec.makeMeasureSpec(
+                View.MeasureSpec.UNSPECIFIED,
+                View.MeasureSpec.UNSPECIFIED
+            ), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        )
+        webView.layout(
+            0, 0, webView.measuredWidth,
+            webView.measuredHeight
+        )
+        webView.isDrawingCacheEnabled = true
+        webView.buildDrawingCache()
+        val bitmap = Bitmap.createBitmap(
+            webView.measuredWidth,
+            webView.measuredHeight, Bitmap.Config.ARGB_8888
+        )
+
+        val canvas = Canvas(bitmap)
+        val paint = Paint()
+        val iHeight = bitmap.height.toFloat()
+        canvas.drawBitmap(bitmap, 0f, iHeight, paint)
+        webView.draw(canvas)
+        val wm = WallpaperManager.getInstance(this)
+        wm.setBitmap(bitmap, Rect(0, 0, bitmap.width, bitmap.height), true, WallpaperManager.FLAG_LOCK)
     }
 
 }
